dad97c6f021ebbd0143b28fe155a483ac60d7737
bluesmilery <Gai@GaideMacBook-Pro.local>
2016-05-30 16:02:22 +0800

    v0.3
    可以根据从屏幕上点选的颜色来改变灯的颜色

--- HueNo1/ControlLightsViewController.swift
+++ HueNo1/ControlLightsViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ControlLightsViewController: UIViewController {
     
+    
+    let lightModel: String = "LCT001"
+    
+    @IBOutlet var iv: UIImageView!
+    var ima: UIImage!
+
     // 存储所有灯的实体
     var allLights: Array<AnyObject>!
 
@@ -50,6 +56,9 @@ class ControlLightsViewController: UIViewController {
         let cache: PHBridgeResourcesCache = PHBridgeResourcesReader.readBridgeResourcesCache()
         allLights = Array(cache.lights.values)
         print("en?")
+        
+        ima = UIImage.init(named: "map-brightness")
+        iv.image = ima
     }
 
     override func didReceiveMemoryWarning() {
@@ -60,7 +69,7 @@ class ControlLightsViewController: UIViewController {
     // 以开关的形式控制灯的亮灭
     func controlLightsOnOrOff(whichLight: Int, whichSwitch: UISwitch) {
         let light = allLights[whichLight-1] as! PHLight
-        let state = light.lightState as PHLightState
+        let state: PHLightState = PHLightState.init()
         state.on = whichSwitch.on
         let bridgeSendAPI: PHBridgeSendAPI = PHBridgeSendAPI.init()
         bridgeSendAPI.updateLightStateForId(light.identifier, withLightState: state) { (error: [AnyObject]!) in
@@ -76,7 +85,7 @@ class ControlLightsViewController: UIViewController {
     func controlLightsBrightness(whichLight: Int, whichSlider: UISlider) {
         whichSlider.continuous = false
         let light = allLights[whichLight-1] as! PHLight
-        let state = light.lightState
+        let state: PHLightState = PHLightState.init()
         state.brightness = Int(whichSlider.value)
         let bridgeSendAPI: PHBridgeSendAPI = PHBridgeSendAPI.init()
         bridgeSendAPI.updateLightStateForId(light.identifier, withLightState: state) { (error: [AnyObject]!) in
@@ -88,6 +97,74 @@ class ControlLightsViewController: UIViewController {
         }
     }
     
+    // 触摸事件
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touchPosition = touches.first!.locationInView(iv)
+        if (touchPosition.x >= 0 && touchPosition.x <= iv.frame.size.width && touchPosition.y >= 0 && touchPosition.y <= iv.frame.size.height) {
+            
+            let realPositionX: CGFloat = touchPosition.x / iv.frame.size.width * ima.size.width
+            let realPositionY: CGFloat = touchPosition.y / iv.frame.size.height * ima.size.height
+            let getColor = ima.getPixelColor(CGPointMake(realPositionX, realPositionY))
+            
+            print("touchesBegan")
+            print(touchPosition)
+            print(CGPointMake(realPositionX, realPositionY))
+            print(getColor)
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touchPosition = touches.first!.locationInView(iv)
+        if (touchPosition.x >= 0 && touchPosition.x <= iv.frame.size.width && touchPosition.y >= 0 && touchPosition.y <= iv.frame.size.height) {
+            
+            let realPositionX: CGFloat = touchPosition.x / iv.frame.size.width * ima.size.width
+            let realPositionY: CGFloat = touchPosition.y / iv.frame.size.height * ima.size.height
+            let getColor = ima.getPixelColor(CGPointMake(realPositionX, realPositionY))
+
+            print("touch")
+            print(touchPosition)
+            print(CGPointMake(realPositionX, realPositionY))
+            print(getColor)
+        }        
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touchPosition = touches.first!.locationInView(iv)
+        if (touchPosition.x >= 0 && touchPosition.x <= iv.frame.size.width && touchPosition.y >= 0 && touchPosition.y <= iv.frame.size.height) {
+            
+            let realPositionX: CGFloat = touchPosition.x / iv.frame.size.width * ima.size.width
+            let realPositionY: CGFloat = touchPosition.y / iv.frame.size.height * ima.size.height
+            let getColor = ima.getPixelColor(CGPointMake(realPositionX, realPositionY))
+            
+            print("touchesEnded")
+            print(touchPosition)
+            print(CGPointMake(realPositionX, realPositionY))
+            print(getColor)
+            
+            let lightColor: UIColor = UIColor.init(red: getColor.red, green: getColor.green, blue: getColor.blue, alpha: getColor.alpha)
+            let rgbToXY = PHUtilities.calculateXY(lightColor, forModel: lightModel)
+            let xOfXY = rgbToXY.x
+            let yOfXY = rgbToXY.y
+            controlLightsColor(xOfColor: xOfXY, yOfColor: yOfXY)
+            print(rgbToXY)
+        }
+    }
+    
+    // 以xy方式改变灯的颜色
+    func controlLightsColor(xOfColor xOfColor: CGFloat, yOfColor: CGFloat) {
+        let light: PHLight = allLights[3-1] as! PHLight
+        let state: PHLightState = PHLightState.init()
+        state.x = xOfColor
+        state.y = yOfColor
+        let bridgeSendAPI: PHBridgeSendAPI = PHBridgeSendAPI.init()
+        bridgeSendAPI.updateLightStateForId(light.identifier, withLightState: state) { (error: [AnyObject]!) in
+            if ((error == nil)) {
+                print("success")
+            } else {
+                print("failure")
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
@@ -99,4 +176,33 @@ class ControlLightsViewController: UIViewController {
     }
     */
 
+    
+}
+
+public extension UIImage {
+    
+    /**
+     获取图片中的像素颜色值
+     
+     - parameter pos: 图片中的位置
+     
+     - returns: 颜色值
+     */
+    func getPixelColor(pos: CGPoint) -> (alpha: CGFloat, red: CGFloat, green: CGFloat, blue: CGFloat) {
+        let pixelData = CGDataProviderCopyData(CGImageGetDataProvider(self.CGImage))
+        let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
+        let pixelInfo: Int = ((Int(self.size.width) * Int(pos.y)) + Int(pos.x)) * 4
+        
+        let r = CGFloat(data[pixelInfo]) / CGFloat(255.0)
+        let g = CGFloat(data[pixelInfo+1]) / CGFloat(255.0)
+        let b = CGFloat(data[pixelInfo+2]) / CGFloat(255.0)
+        let a = CGFloat(data[pixelInfo+3]) / CGFloat(255.0)
+//        let r = CGFloat(data[pixelInfo])
+//        let g = CGFloat(data[pixelInfo+1])
+//        let b = CGFloat(data[pixelInfo+2])
+//        let a = CGFloat(data[pixelInfo+3])
+        
+        return (a,r,g,b)
+    }
+    
 }
